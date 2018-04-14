b2b5e1ee0024444ebc44cb80e01c35ab80faef64
wuqingyi <254357367@qq.com>
2018-04-14 17:18:45 +0800

    Initial commit

---
+++ src/main/java/com/wqy/starterhello/autoconfigs/HelloAutoConfig.java
@@ -0,0 +1,28 @@
+package com.wqy.starterhello.autoconfigs;
+
+import com.wqy.starterhello.ServiceProperties.HelloServiceProperties;
+import com.wqy.starterhello.services.HelloService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigurationProperties(HelloServiceProperties.class)
+@ConditionalOnClass(HelloService.class)
+@ConditionalOnProperty(prefix = "hello", value = "enabled", matchIfMissing = true)
+public class HelloAutoConfig {
+    @Autowired
+    private HelloServiceProperties helloServiceProperties;
+
+    @Bean
+    @ConditionalOnMissingBean(HelloService.class)
+    public HelloService helloService(){
+        HelloService helloService = new HelloService();
+        helloService.setMsg(helloServiceProperties.getMsg());
+        return helloService;
+    }
+}
---
+++ src/main/java/com/wqy/starterhello/services/HelloService.java
@@ -0,0 +1,16 @@
+package com.wqy.starterhello.services;
+
+public class HelloService {
+    private String msg;
+    public String  sayHello(){
+        return "Hello" + msg;
+    }
+
+    public String getMsg(){
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+}
